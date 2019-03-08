c1e3c2b832d6392f7feb9d847d7a0d319716c182
ayrat-l <kaz90@bk.ru>
2019-03-08 13:57:34 +0300

    Best sales store calculation added

---
+++ app/main.py
@@ -0,0 +1,16 @@
+#Лучшие продажи у магазина
+
+stores = [
+    [500, 700, 400, 540, 610, 1100, 800],
+    [300, 490, 500, 320, 600, 900, 1500],
+    [600, 140, 250, 400, 1300, 790, 2000]
+]
+
+store_profit = [] #Общий доход каждого магазина
+for store in stores:
+    store_profit.append(sum(store))
+
+top_store_index = [] #Находим индекс лучшего магазина по суммарной выручке
+for index, profit in enumerate(store_profit):
+    if profit == max(store_profit):
+        top_store_index.append(index)
